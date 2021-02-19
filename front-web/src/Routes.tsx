5967bde9257506034f2fed822b6a8cf50eeecd50
vcaselli <vcasellidev@gmail.com>
2021-02-19 11:14:51 -0300

    news

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,30 +1,31 @@
-import React from 'react'; 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './core/components/navbar';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
+import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 
-const Routes = () => ( 
-    <BrowserRouter>
+
+const Routes = () => (
+  <BrowserRouter>
     <Navbar />
     <Switch>
-        <Route path="/" exact>
-            <Home/>
-        </Route>
-
-        <Route path="/catalog">
-            <Catalog />
-        </Route>
-
-
-        <Route path="/admin">
-            <Admin />
-        </Route>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/products" exact>
+        <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
+      </Route>
+      <Redirect from="/admin" to="/admin/products" exact />
+      <Route path="/admin">
+        <Admin />
+      </Route>
     </Switch>
-    
-    </BrowserRouter>
-); 
-
+  </BrowserRouter>
+);
 
 export default Routes;
